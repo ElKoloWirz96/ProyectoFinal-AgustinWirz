d9ccb080ddb74a12d530b93124677bd25271e333
ElKoloWirz96 <agustin-96@live.com.ar>
2024-04-04 16:53:21 -0300

    Proyecto inicial empezado

---
+++ src/app/layouts/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  showFiller = false;
+
+//  toggleContent() {
+//    this.showFiller = !this.showFiller;
+//  }
+};
+
